d0aa24a4962db955e9ae65d09165be4c293cb144
Vo Tran Phi <songokupkj@gmail.com>
2025-05-05 03:31:17 +0700

    refactor(ot-47): replace projects and package.json files

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,28 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'YuilRin*123',
-    database: 'octaltask',
-    entities: [User],
-    synchronize: true, // true khi dev
-  }),
-  TypeOrmModule.forFeature([User]),
-  UsersModule,
-],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-

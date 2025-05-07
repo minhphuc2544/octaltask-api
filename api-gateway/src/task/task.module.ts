e7cbcb03fd87b9630d519ea5a35a92e229d031a5
Vo Tran Phi <songokupkj@gmail.com>
2025-05-07 23:25:17 +0700

    refactor(ot-40): bring client (api-gateway) url connection to .env

--- api-gateway/src/task/task.module.ts
+++ api-gateway/src/task/task.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { ClientsModule } from '@nestjs/microservices';
-import { grpcClientOptions } from 'src/client-options/task.grpc-client';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getTaskGrpcClientOptions } from 'src/client-options/task.grpc-client';
 
 @Module({
   imports: [
-      ClientsModule.register([grpcClientOptions]),
-    ],
+    ClientsModule.registerAsync([
+      {
+        name: 'TASK_PACKAGE',
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: getTaskGrpcClientOptions,
+      },
+    ]),
+  ],
   providers: [TaskService],
   controllers: [TaskController],
   exports: [TaskService],

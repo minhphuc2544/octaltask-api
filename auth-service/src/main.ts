c478c2462493de888a943c5ead35f1ab86c5ae96
Vo Tran Phi <songokupkj@gmail.com>
2025-05-07 22:58:53 +0700

    refactor(ot-40): bring gRPC's urls to .env

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -2,16 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const configService = appContext.get(ConfigService);
+
+  const grpcUrl = configService.get<string>('GRPC_AUTH_URL');
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.GRPC,
     options: {
       package: 'auth',
       protoPath: join(__dirname, './proto/auth.proto'),
-      url: '0.0.0.0:50051',
+      url: grpcUrl,
     },
   });
+
   await app.listen();
 }
 bootstrap();

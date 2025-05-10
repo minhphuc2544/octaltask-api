74326653d91e8d822f65308f0dedfdff9273f73f
Vo Tran Phi <songokupkj@gmail.com>
2025-05-10 20:39:25 +0700

    refactor(ot-48): remove unnecessary endpoint

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,14 @@ async function bootstrap() {
   }));
   
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Octaltask API Documentation')
+    .setDescription('Documentation for Octaltask application')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('SERVER_PORT') || 3000;
